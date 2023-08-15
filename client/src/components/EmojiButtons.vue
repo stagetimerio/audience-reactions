931fb0210cdd17a6c826f817c7236d579a44e9ea
Lukas Hermann <hey@lukashermann.dev>
2023-08-15 12:52:53 +0200

    feat: add product hunt button

--- client/src/components/EmojiButtons.vue
+++ client/src/components/EmojiButtons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col gap-3">
     <div
       class="flex-1 grid grid-cols-2 md:grid-cols-3 gap-3"
     >
@@ -11,6 +11,22 @@
       >
         {{ keyEmojiMap[key] }}
       </button>
+    </div>
+    <div>
+      <a
+        class="block mx-auto"
+        style="width: 200px;"
+        href="https://www.producthunt.com/posts/audience-reacts?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-audience&#0045;reacts"
+        target="_blank"
+        rel="noopener"
+      >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=409696&theme=light"
+          alt="Audience&#0032;Reacts - Feel&#0032;the&#0032;room&#0044;&#0032;one&#0032;emoji&#0032;at&#0032;a&#0032;time | Product Hunt"
+          width="250"
+          height="54"
+        >
+      </a>
     </div>
   </div>
 </template>
