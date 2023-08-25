edc3c11b7d2b789281ed300262ae622303319198
Lukas Hermann <hey@lukashermann.dev>
2023-08-25 21:53:51 +0200

    feat: added 600ms button cooldown

--- client/src/components/EmojiButtons.vue
+++ client/src/components/EmojiButtons.vue
@@ -6,40 +6,46 @@
       <button
         v-for="key in Object.values(emoteKeys)"
         :key="key"
-        class="rounded-lg hover:bg-white shadow-lg text-5xl"
+        class="rounded-lg shadow-lg text-5xl"
         :class="{
           'bg-black': props.fullOpacity,
           'bg-black/30': !props.fullOpacity,
+          'hover:bg-white': !cooldown,
         }"
-        @click="emit('trigger-emote', key)"
+        :disabled="cooldown"
+        @click="onClick(key)"
       >
         {{ keyEmojiMap[key] }}
       </button>
     </div>
-    <!-- <div>
-      <a
-        class="block mx-auto"
-        style="width: 200px;"
-        href="https://www.producthunt.com/posts/audience-reacts?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-audience&#0045;reacts"
-        target="_blank"
-        rel="noopener"
-      >
-        <img
-          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=409696&theme=light"
-          alt="Audience&#0032;Reacts - Feel&#0032;the&#0032;room&#0044;&#0032;one&#0032;emoji&#0032;at&#0032;a&#0032;time | Product Hunt"
-          width="250"
-          height="54"
-        >
-      </a>
-    </div> -->
+    <div
+      v-if="cooldown"
+      class="absolute z-20 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-black/30 rounded-lg p-4 backdrop-blur"
+    >
+      <div class="animate-spin w-8 h-8 leading-8 text-center text-3xl">
+        ⏳
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { emoteKeys, keyEmojiMap } from '../store/emotes.js'
+import { ref } from 'vue'
 
+const cooldown = ref(false)
 const emit =  defineEmits(['trigger-emote'])
 const props = defineProps({
   fullOpacity: Boolean,
 })
+
+function onClick (key) {
+  emit('trigger-emote', key)
+  startCooldown()
+}
+
+function startCooldown () {
+  cooldown.value = true
+  setTimeout(() => (cooldown.value = false), 600)
+}
 </script>
